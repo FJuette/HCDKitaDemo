11506b613679e35364f24042c9d6c74ef6d57a86
Fabian <Fabian.Juette@tu-clausthal.de>
2017-01-09 10:17:51 +0100

    Page for staff

--- staff.php
+++ staff.php
@@ -15,7 +15,40 @@
             <?php include("navigation.php") ?>
             <div id="content" class="col-md-7 innerPadding">
                 <h4 class="about">Mitarbeiter/innen</h4>
-                <h5>Unser Motto: „Naturnah, kreativ und bewegungsfroh“</h5>
+                <p>Derzeit sind es zehn Erzieher/innen und pädagogische Mitarbeiter/innen, die die Kinder der KiTa Erzstraße willkommen heißen.</p>
+                <ul class="staffList">
+                    <li>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img class="staffImg" src="images/staff1.png">
+                            </div>
+                            <div class="col-md-8" style="margin-top: 50px;">
+                                <b class="about">Heidrun Meier</b>
+                                <br />
+                                Erzieherin
+                                <br />
+                                Schwerpunkt: Natur- und Waldpädagogik
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img class="staffImg" src="images/staff2.png">
+                            </div>
+                            <div class="col-md-8" style="margin-top: 50px;">
+                                <b class="about">Sibylle Block</b>
+                                <br />
+                                Erzieherin
+                                <br />
+                                Schwerpunkt: Förderungspädagogik
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        ...
+                    </li>
+                </ul>
 
             </div>
         </div>
